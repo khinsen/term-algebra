9672b9ea57536f0287a6b36a768dd5194229ad3e
Konrad Hinsen <konrad.hinsen@fastmail.net>
2015-01-07 12:22:18 +0100

    Better data structure for overloaded operators

--- term-algebra/operators.rkt
+++ term-algebra/operators.rkt
@@ -2,11 +2,11 @@
 
 (provide empty-op-set
          add-op
-         (struct-out op))
+         (struct-out operator))
 
 (require (prefix-in sorts: term-algebra/sorts))
 
-(struct op (symbol domain range properties)
+(struct operator (symbol signatures properties)
         #:transparent)
 
 ; Page 66: To avoid ambiguous expressions we require that if the sorts
@@ -18,37 +18,40 @@
 ; Management of op-sets
 ;
 ; An op-set is stored as a hash mapping symbols to overloaded
-; operators. An overloaded operator is represented as a hash
-; mapping argument kinds to lists of individual operators.
-; These lists are sorted by increasing domain sort, such that
-; operator lookup can proceed linearly.
+; operators. An overloaded operator (struct operator) has a single
+; symbol and a single set of properties plus a list of signatures.
+; Each signature entry is a pair of a domain (a list of sorts)
+; and a range (a single sort). Signatures are sorted by increasing
+; range sort, such that operator lookup can proceed linearly.
 ;
 
 (define (empty-op-set)
   (hash))
 
-(define (add-op op sorts op-set)
-  (let* ([symbol (op-symbol op)]
-         [domain-kinds (for/list ([s (op-domain op)]) (sorts:kind s sorts))]
-         [range-kind (sorts:kind (op-range op) sorts)]
+(define (add-op symbol domain range properties sorts op-set)
+  (let* ([domain-kinds (map (λ (s) (sorts:kind s sorts)) domain)]
+         [range-kind (sorts:kind range sorts)]
          [ops-for-symbol (hash-ref op-set symbol (hash))]
-         [ops-for-kinds (hash-ref ops-for-symbol domain-kinds empty)])
-    (unless (empty? ops-for-kinds)
-      ; Since ops are added one by one, it is sufficient to check
-      ; compatibility with the first existing one.
-      (let ([opk (first ops-for-kinds)])
-        (unless (equal? (op-properties op) (op-properties opk))
-          (error (format "Operator ~s must have properties ~s"
-                         op (op-properties opk))))
-        (unless (equal? range-kind (sorts:kind (op-range opk) sorts))
-          (error (format "Operator ~s must have the kind of sort ~s"
-                         op (op-range opk))))))
-    (define-values (before after)
-      (splitf-at ops-for-kinds (λ (opk) (sorts:is-subsort?
-                                         (op-range opk) (op-range op) sorts))))
-    (hash-set op-set
-              symbol (hash-set ops-for-symbol
-                               domain-kinds  (append before (cons op after))))))
-
-; Better define
-; (struct overloaded-op (domain-kinds) range-kind properties)
+         [op (hash-ref ops-for-symbol domain-kinds #f)])
+    (let ([extended-op
+           (if op
+               (let* ([op-signatures (operator-signatures op)]
+                      [first-signature (first op-signatures)]
+                      [first-range (cdr first-signature)])
+                 (unless (equal? properties (operator-properties op))
+                   (error (format "Operator ~s must have properties ~s"
+                                  symbol (operator-properties op))))
+                 (unless (equal? range-kind (sorts:kind first-range sorts))
+                   (error (format "Operator ~s must have the kind of sort ~s"
+                                  symbol first-range)))
+                 (define-values (before after)
+                   (splitf-at op-signatures
+                              (λ (sig) (sorts:is-subsort?
+                                        (cdr sig) range sorts))))
+                 (operator symbol (append before
+                                          (cons (cons domain range) after))
+                           properties))
+               (operator symbol (list (cons domain range)) properties))])
+      (hash-set op-set
+                symbol (hash-set ops-for-symbol
+                                 domain-kinds extended-op)))))
--- tests/test-operators.rkt
+++ tests/test-operators.rkt
@@ -14,45 +14,59 @@
   (subsorts [A C] [B C] [X Y] [Y Z]))
 
 (define test-sorts (module-sorts test))
+(define A-kind (sorts:kind 'A test-sorts))
+(define X-kind (sorts:kind 'X test-sorts))
 
 (define test-ops
   (let* ([ops (operators:empty-op-set)]
          [ops (operators:add-op
-               (operators:op 'foo (list 'A) 'X (set))
+               'foo (list 'A) 'X (set)
                test-sorts ops)]
          [ops (operators:add-op
-               (operators:op 'foo (list 'C) 'Z (set))
+               'foo (list 'C) 'Z (set)
                test-sorts ops)])
     ops))
 
 (define-test-suite operator-tests
   
+  (test-case "test-sorts"
+    (check-equal? A-kind (set 'A 'B 'C))
+    (check-equal? X-kind (set 'X 'Y 'Z)))
+  
   (test-case "operator-definition"
 
     (check-equal? (hash-count (hash-ref (operators:add-op
-                                         (operators:op 'foo (list 'Z) 'A (set))
+                                         'foo (list 'Z) 'A (set)
                                          test-sorts test-ops)
                                         'foo))
                   2)
     (check-equal? (hash-count (hash-ref (operators:add-op
-                                         (operators:op 'foo (list 'B) 'Z (set))
+                                         'foo (list 'B) 'Z (set)
                                          test-sorts test-ops)
                                         'foo))
-                  1))
+                  1)
+    (check-equal? (length
+                   (operators:operator-signatures
+                    (hash-ref (hash-ref (operators:add-op
+                                         'foo (list 'B) 'Z (set)
+                                         test-sorts test-ops)
+                                        'foo)
+                              (list A-kind))))
+                  3))
   
   (test-exn "wrong-range-kind"
       #rx"Operator .*foo.* must have the kind of sort.*"
-    (lambda () 
-      (operators:add-op
-       (operators:op 'foo (list 'B) 'A (set))
-       test-sorts test-ops)))
+      (lambda () 
+        (operators:add-op
+         'foo (list 'B) 'A (set)
+         test-sorts test-ops)))
 
   (test-exn "mixed-var-args"
       #rx"Operator .*foo.* must have properties .*"
-    (lambda () 
-      (operators:add-op
-       (operators:op 'foo (list 'B) 'Z (set 'variable-length-domain))
-       test-sorts test-ops))))
+      (lambda () 
+        (operators:add-op
+         'foo (list 'B) 'Z (set 'variable-length-domain)
+         test-sorts test-ops))))
 
 (module* main #f
   (require rackunit/text-ui)
